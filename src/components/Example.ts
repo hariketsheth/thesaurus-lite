477d5aaf83e775c004d9847be2db8942fca83888
Hariket Sukeshkumar Sheth <SHETHHARIKET@gmail.com>
2022-07-05 15:07:48 +0530

    Added `Example`

---
+++ src/components/Example.ts
@@ -0,0 +1,9 @@
+export const info = {
+  hide: false,
+  title: 'Thesaurus Lite',
+  description: 'Share the link to play this game live with others. Open it in a new window to try it against yourself.',
+  githubHref: 'https://github.com/hariketsheth',
+  codeSandboxHref: '',
+  twitterHref: 'https://twitter.com/HariketSheth',
+  website: 'https://hariketsheth.github.io'
+}
